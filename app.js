e9f95fa540595bf527e9f11de2e8e5496804c7ce
rubenhillo <rho5991388@hotmail.com>
2021-11-19 21:45:35 -0600

    Primera modificaicon

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.use(express.static(__dirname + '/public'));
-console.log(__dirname + './public');
+console.log(__dirname + '/public');
 
 app.listen(3000, ()=>{
     console.log('Servidor funcionando');
