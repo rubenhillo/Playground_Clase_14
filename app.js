5458415bb7bb4bf8de317067ffd367f0e64aec2a
rubenhillo <rho5991388@hotmail.com>
2021-11-19 21:41:41 -0600

    Solucion Tutores - Material de ejercitación clase 14

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+app.use(express.static(__dirname + '/public'));
+console.log(__dirname + './public');
+
+app.listen(3000, ()=>{
+    console.log('Servidor funcionando');
+
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
